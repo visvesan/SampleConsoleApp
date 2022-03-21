63826bde3773d31ad9962b7fff4d3525d3b70eed
Kulasekaralwar Chandiran <kulasekaralwar.chandiran@hidglobal.com>
2022-03-21 09:54:51 +0530

    small change

--- SampleConsoleApp/Program.cs
+++ SampleConsoleApp/Program.cs
@@ -7,6 +7,7 @@ namespace SampleConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(3 + 5);
             Console.ReadLine();
         }
     }
