bc82bcb9f4e7ac5cba4a9c7d5af57e3f43d222fd
Kulasekaralwar Chandiran <kulasekaralwar.chandiran@hidglobal.com>
2022-03-21 17:21:05 +0530

    d

--- SampleConsoleApp/Program.cs
+++ SampleConsoleApp/Program.cs
@@ -10,6 +10,7 @@ namespace SampleConsoleApp
             Console.WriteLine(3 + 5);
             Console.WriteLine(6 + 6);
             Console.WriteLine(7 + 7);
+            Console.WriteLine("To Test");
             Console.ReadLine();
         }
     }
